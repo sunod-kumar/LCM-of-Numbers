37d83f0b03250d654833c857d60f5a81f4e58614
sunod-kumar <sunod222057@cdlsiet.ac.in>
2024-02-14 20:19:49 +0530

    commenting the python

--- LCM.py
+++ LCM.py
@@ -1,22 +1,24 @@
 from math import *
 
 
-def lcm(numbers):
+def lcm(numbers):  # function for finding the LCM
     lcm = numbers[0]
     for num in numbers[1:]:
         lcm = lcm * num // gcd(lcm, num)
     return lcm
 
-def gcd(number_1, number_2):
+
+def gcd(number_1, number_2):  # function for finding GCD(greatest common divisor)
     while number_2:
         number_1, number_2 = number_2, number_1 % number_2
     return number_1
 
+
 def main():
     numbers = []
-    while True:
+    while True:  #inputing the integers values which we want to find HCF and LCM.
         try:
-            number = int(input('Enter the number: '))
+            number = int(input("Enter the number: "))
             if number == 0:
                 break
             else:
@@ -24,8 +26,7 @@ def main():
         except:
             # print(f"Invalid Input")
             pass
-    print(f'LCM of the {numbers}: ', lcm(numbers))
-
+    print(f"LCM of the {numbers}: ", lcm(numbers))
 
 
 if __name__ == "__main__":
